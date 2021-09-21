64a8cc0dc2e009e0e8d43e8319cc3c8def5deb2d
kebbet <info@verkan.se>
2021-09-21 19:34:18 +0200

    Sort vars and rename vars in replace_content_with_sup()

--- src/shortcode.php
+++ src/shortcode.php
@@ -21,28 +21,30 @@ defined( 'ABSPATH' ) or exit;
 function replace_content_with_sup( $attributes, $content ) {
 
 	// Count number of notes in each post.
-	global $footnote_count;
+	global $counter;
 
 	$attributes  = '';
-	$sup_id      = '';
-	$target_post = \kebbet\shortcode\footnotes\helpers\get_post_scope_id();
-	$content     = do_shortcode( $content ); // Render out any shortcode within the contents.
-	$first_item  = ! isset( $footnote_count[$target_post] );
 	$note_number = intval( 1 );
+	$sup_class   = apply_filters( 'kebbet_shortcode_footnote_note_class', 'footnotes-footnote' );
+	$sup_id      = '';
+	$target      = \kebbet\shortcode\footnotes\helpers\get_post_scope_id();
+	$first_item  = ! isset( $counter[$target] );
 
 	// Which footnote is this?
 	if ( ! $first_item ) {
-		$find_max    = max( $footnote_count[$target_post]['ref'] );
+		$find_max    = max( $counter[$target]['ref'] );
 		$note_number = $find_max + intval( 1 );
 	}
 
-	$footnote_count[$target_post]['ref'][] = $note_number;
+	$counter[$target]['ref'][] = $note_number;
+	$note_link                 = \kebbet\shortcode\footnotes\helpers\link_id( $note_number, true, true );
 
 	// Add optional title attribute to link element.
 	if ( true === \kebbet\shortcode\footnotes\settings\title_attributes() ) {
+		$content    = do_shortcode( $content ); // Render out any shortcode within the contents.
 		$content    = \kebbet\shortcode\footnotes\helpers\strip_paragraph( $content );
-		$title      = str_replace( '"', '&quot;', strip_tags( $content ) );
-		$attributes = ' title="' . esc_attr( $title ) . '"';
+		$content    = str_replace( '"', '&quot;', strip_tags( $content ) );
+		$attributes = ' title="' . esc_attr( $content ) . '"';
 	}
 
 	// Add back links if enabled.
@@ -51,16 +53,10 @@ function replace_content_with_sup( $attributes, $content ) {
 		$sup_id    = ' id="' . esc_attr( $source_id ) . '"';
 	}
 
-	$sup_class = apply_filters( 'kebbet_shortcode_footnote_note_class', 'footnotes-footnote' );
-	$note_link = \kebbet\shortcode\footnotes\helpers\link_id( $note_number, true, true );
-
 	// Build the `sup`-element.
 	$sup_content  = '<sup' . $sup_id . ' class="' . esc_attr( $sup_class ) . '">';
 	$sup_content .= '<a href="' . esc_url( $note_link ) . '"' . $attributes . '>' . esc_attr( $note_number ) . '</a>';
 	$sup_content .= '</sup>';
-
-	// Replace the content between shortcode with the new <sup>-number.
-	$content = $sup_content;
 
 	return $content;
 }
